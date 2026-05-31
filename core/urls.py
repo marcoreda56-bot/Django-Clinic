70b11d930482adf3a3168bdda7ab68c507772d09
mahmoudrabbas <mahmoudrabass11@gmail.com>
2026-05-31 14:32:46 +0300

    fixing/login

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.split if hasattr(admin.site, 'split') else admin.site.urls), 
+    path('admin/', admin.site.urls),
     path('api/', include('clinic.urls')),
 ]
